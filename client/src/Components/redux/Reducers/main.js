f58cf3226a12c8e893244f9aab8cbbfcfd50ad83
Sourabh Anil Pote <110386969+SourabhPote007@users.noreply.github.com>
2024-05-11 13:13:18 +0530

    Delete client directory

--- client/src/Components/redux/Reducers/main.js
+++
@@ -1,8 +0,0 @@
-import { getProductsreducer } from "./productreducer";
-import {combineReducers} from "redux";
-
-const rootreducers = combineReducers({
-    getProductsdata:getProductsreducer
-});
-
-export default rootreducers;
